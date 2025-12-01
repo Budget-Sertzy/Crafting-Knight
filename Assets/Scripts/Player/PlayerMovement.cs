03eacc6898e3773f4690effca1a6c78c09d0269d
Sertzy <evan7885@gmail.com>
2025-12-01 16:44:36 -0500

    Switched versions for easy of turning it in

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,59 @@
+using System;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+
+    public float drag;
+
+    public Transform orientation;
+
+    private float horizontalInput;
+    private float verticalInput;
+
+    private Vector3 moveDirection;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+        
+    }
+
+    private void Update()
+    {
+        UserInput();
+        MovePlayer();
+        SpeedControl();
+
+        rb.linearDamping = drag;
+    }
+
+    private void UserInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+
+    void MovePlayer()
+    {
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Acceleration);
+        
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+
+        if (flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.linearVelocity = new Vector3(limitedVel.x, rb.linearVelocity.y, limitedVel.z);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerSettings.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerSettings : MonoBehaviour
+{
+    
+   
+
+    private void Update()
+    {
+        
+    }
+
+   
+}
